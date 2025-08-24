9c22c25c5900e61bb2bfae68b72d47f9a1f3efcd
Nehla <nao.heb@gmail.com>
2025-08-24 23:55:21 +0000

    Split KB OLED tests

---
+++ keyboards/bbtest/config.h
@@ -0,0 +1,11 @@
+#pragma once
+// OLED
+#define OLED_DISPLAY_128X64
+
+#define I2C_DRIVER_ENABLE
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN GP3
+#define I2C1_SDA_PIN GP2
+
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
