e23c83fabd5c3bcb1d55cb345f8efb2e795c9581
Nehla <nao.heb@gmail.com>
2025-09-19 00:18:28 +0000

    Made the test version for noodle_split

--- keyboards/noodle_split/keymaps/default/keymap.c
+++ keyboards/noodle_split/keymaps/default/keymap.c
@@ -17,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤Tab│   │Spc├───┘
       *                   └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_2(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_N,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_BSPC, KC_TAB
+    [0] = LAYOUT_split_4x6_2(
+        KC_NO,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
+        KC_Y,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_Y,
+        KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LCTL, KC_LGUI, KC_BSPC, KC_ALGR, KC_NO,   KC_NO,   KC_NO,   KC_NO
     )
 };
 
